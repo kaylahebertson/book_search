eac4f2b03bdcb518865bbbac32c504cc9413b5ac
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-09 21:08:20 -0700

    change auth to grahpql

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'node:path';
+import type { Request, Response } from 'express';
 import db from './config/connection.js';
 import { ApolloServer } from '@apollo/server';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { expressMiddleware } from '@apollo/server/express';
-import { authenticateToken } from './services/auth-service.js';
+import { authenticateToken } from './services/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -29,7 +30,7 @@ const startServer = async () => {
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (_req:, res) => {
+  app.get('*', (_req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
